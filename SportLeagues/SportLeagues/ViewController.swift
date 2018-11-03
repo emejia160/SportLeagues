0f8627ed9dd00814d273f366d81cce70fa86061a
eddmejia <eddmejia@bancolombia.com.co>
2018-11-03 10:50:37 -0500

    Created xcode project

---
+++ SportLeagues/SportLeagues/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SportLeagues
+//
+//  Created by Edison Mejia Estrada on 3/11/18.
+//  Copyright © 2018 Edison Mejia Estrada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
