9d84e7362739b5281a85a556eb1ef58e176acd48
eddmejia <eddmejia@bancolombia.com.co>
2018-11-03 12:20:35 -0500

    Added icon image and created constants file

---
+++ SportLeagues/SportLeagues/Resources/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  SportLeagues
+//
+//  Created by Edison Mejia Estrada on 3/11/18.
+//  Copyright © 2018 Edison Mejia Estrada. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var baseUrl: String {
+        get {
+            return Bundle.main.object(forInfoDictionaryKey: "apiBaseUrl") as! String
+        }
+    }
+    
+    struct Request {
+        
+        static let listTeamsPerLeaguePath = baseUrl + "/lookup_all_teams.php?id=%@"
+        static let listAllLeaguesPath = baseUrl + "/all_leagues.php"
+        
+    }
+    
+}
